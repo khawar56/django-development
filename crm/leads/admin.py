4ec3698895f5928480ac0a577b9cbf3a8290297c
Khawar <khawar890@gmail.com>
2021-09-05 01:12:30 +0500

    models created

---
+++ crm/leads/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Agent, Lead, User
+
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
+# Register your models here.
